d5a473f59a9e77c187e17579979353df4599a053
rxuriguera <rxuriguera@gmail.com>
2010-01-27 00:45:40 +0100

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,4 @@ setup(
     # basic package data
     name = "TestApp",
     version = "0.0",
-    # package structure
-    packages=find_packages('source'),
-    package_dir={'':'source'},
-    entry_points = {
-    'console_scripts': [
-            'testApp = bimaker.app:main'
-            ]
-    },
 )
