0ae038e13aa75a5e584c943f341efb8630773887
rxuriguera <rxuriguera@gmail.com>
2010-01-27 00:44:10 +0100

    Use ez_setup to install setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup, find_packages, use_setuptools
+from ez_setup import  use_setuptools
 use_setuptools(version='0.6c11')
+
+from setuptools import setup, find_packages
 setup(
     # basic package data
     name = "TestApp",
