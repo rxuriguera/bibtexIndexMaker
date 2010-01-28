f00996df95f2b1a2d50e367d1f5859d18bab0d3e
rxuriguera <rxuriguera@gmail.com>
2010-01-28 02:04:48 +0100

    Remove setup files from root directory

--- setup.py
+++
@@ -1,9 +0,0 @@
-from ez_setup import  use_setuptools
-use_setuptools(version='0.6c11')
-
-from setuptools import setup, find_packages
-setup(
-    # basic package data
-    name = "BibtexIndexMaker",
-    version = "0.0",
-)
