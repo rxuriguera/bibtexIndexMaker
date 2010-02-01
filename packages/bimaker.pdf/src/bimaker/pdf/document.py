29dc87e41bdc64764502b1e2686543ef0f977386
rxuriguera <rxuriguera@gmail.com>
2010-02-01 11:39:32 +0100

    Add pdf package scripts

---
+++ packages/bimaker.pdf/src/bimaker/pdf/document.py
@@ -0,0 +1,19 @@
+
+class Document(object):
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        pass
+    
+
+class Article(Document):
+    """
+    """
+    
+    def __init__(self):
+        pass
+
+    
