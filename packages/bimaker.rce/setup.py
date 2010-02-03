aa1d722b72c3de5ebbc319ff94fd82a8491f5764
rxuriguera <rxuriguera@gmail.com>
2010-02-03 17:47:15 +0100

    Rename module from pdf to rce
    In an attempt to make things more general, module pdf has been renamed to
    rce (for Raw Content Extraction).

---
+++ packages/bimaker.rce/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='bimaker.rce',
+      version=version,
+      description="BibtexIndexMaker raw content extraction",
+      license='GPL',
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      install_requires=['setuptools'],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
+
