ef89cfd2919e096839302f462bebd979f32b641b
rxuriguera <rxuriguera@gmail.com>
2010-02-03 20:13:20 +0100

    Rename top-level package name to bibim
    This is due to other software with similar names.

---
+++ packages/bibim.ir/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='bibim.ir',
+      version=version,
+      description="BibtexIndexMaker Information Retrieval package",
+      license='GPL',
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      install_requires=['setuptools'],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
+
