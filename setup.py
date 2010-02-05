cd6f3c223140c3c2aa6dc47133a2328e1c1e2b3c
rxuriguera <rxuriguera@gmail.com>
2010-02-05 15:14:12 +0100

    Update setup.py to include tools and configuration when creating the project egg.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='bibim',
       version=version,
       description="Bibtex Bibliography Index Maker",
       license='GPL',
-      packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      packages = find_packages('src',exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       package_dir = {'':'src'},
       install_requires=['setuptools'],
       entry_points="""
