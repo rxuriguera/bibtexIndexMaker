25846c43750770872b5c479fc2a0c9af5340317b
rxuriguera <rxuriguera@gmail.com>
2010-02-12 12:12:02 +0100

    Add references.format structure

---
+++ src/bibim/references/format/formatter.py
@@ -0,0 +1,34 @@
+
+# Copyright 2010 Ramon Xuriguera
+#
+# This file is part of BibtexIndexMaker.
+#
+# BibtexIndexMaker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BibtexIndexMaker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BibtexIndexMaker. If not, see <http://www.gnu.org/licenses/>.
+ 
+
+class ReferenceFormatter(object):
+
+    def __init__(self):
+        pass
+    
+    def format_reference(self, reference, format_generator):
+        #Idea
+        #entry = ""
+        #entry += format_generator.generate_header()
+        #for field in reference.fields:
+            #generate_method = 'generate_' + field.name
+            #entry += format_generator.generate_method(field.value)
+        #entry += format_generator.generate_footer()
+        #reference.entry = entry
+        pass
