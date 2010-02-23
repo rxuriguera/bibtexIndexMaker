375b070894c62d2b829200d8611b00affc5509ef
rxuriguera <rxuriguera@gmail.com>
2010-02-24 00:59:18 +0100

    Rename bibim.py to entry.py
    This file will include entry points to the application logic.
    Moreover, FileManager has been removed from this file

--- src/bibim/main/entry.py
+++ src/bibim/main/entry.py
@@ -16,12 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with BibtexIndexMaker. If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import #@UnresolvedImport
+"""
+Entry points to the application
+"""
 
-import os.path
 import Queue #@UnresolvedImport
 
-from bibim.main import ThreadRunner, ReferenceMakerThread
+from bibim.main.files import FileManager
+from bibim.main.threads import ThreadRunner
+
 '''                        
 pdf_extractor = PDFTextExtractor()
 ref_wrapper = ReferenceWrapper()
@@ -83,53 +86,3 @@ class IndexMaker(object):
         thread_runner = ThreadRunner(self._in_queue, self._out_queue)
         thread_runner.run()
         
-        
-
-        
-        
-
-class FileManager(object):  
-    def get_files_list(self, path, ext=None):
-        path = os.path.normpath(path)
-        self.check_path(path)
-        files = []
-        if os.path.isfile(path):
-            files.append(path)
-        elif os.path.isdir(path):
-            if ext:
-                files.extend(self.dir_entries(path, ext))
-            else:
-                files.extend(self.dir_entries(path))
-        return files
-        
-    def check_path(self, path):
-        if not os.access(path, os.W_OK):
-            raise IOError('No access to path')
-        
-    def check_extension(self, path, ext):
-        return path.endswith('.' + ext)
-
-    def dir_entries(self, dir, *args):
-        '''
-        Return a list of file names found in directory 'dir'
-        If there are no additional arguments, all files found in the directory 
-        are added to the list.
-        Example usage: fileList = dir_entries(r'/home', 'txt', 'py')
-        Only files with 'txt' and 'py' extensions will be added to the list.
-        Example usage: fileList = dir_entries(r'/home', True)
-        All files and all the files in subdirectories under /home will be 
-        added to the list.
-        '''
-        fileList = []
-        for file in os.listdir(dir):
-            dirfile = os.path.join(dir, file)
-            if os.path.isfile(dirfile):
-                if not args:
-                    fileList.append(dirfile)
-                else:
-                    if os.path.splitext(dirfile)[1][1:] in args:
-                        fileList.append(dirfile)
-            elif os.path.isdir(dirfile):
-                fileList.extend(self.dir_entries(dirfile, *args))
-        return fileList
-
