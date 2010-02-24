504faf6f52eef7b677c26016c6653b147dfaf845
rxuriguera <rxuriguera@gmail.com>
2010-02-24 01:14:14 +0100

    Add make_reference method to ReferenceMaker

--- src/bibim/main/refmaker.py
+++ src/bibim/main/refmaker.py
@@ -16,11 +16,40 @@
 # You should have received a copy of the GNU General Public License
 # along with BibtexIndexMaker. If not, see <http://www.gnu.org/licenses/>.
 
+from bibim.util.helpers import FileFormat
+from bibim.main.factory import UtilFactory
+from bibim.main.controllers import (RCEController,
+                                    IRController,
+                                    IEController)
 
 class ReferenceMaker(object):
+    def __init__(self):
+        self.factory = UtilFactory()
     
-    def __init__(self, file, source_format, targe_format):
-        pass
-
-    def make_reference(self, file):
-        pass
+    def make_reference(self, file, target_format):
+        """
+        Uses the controllers to extract the content of a file, get some query
+        strings, retrieve results from a search engine, and extract the
+        reference.
+        """
+        entries = []
+        
+        rce = RCEController(self.factory)
+        content = rce.extract_content(file, FileFormat.TXT)
+        if not content:
+            return entries
+        
+        query_strings = rce.get_query_strings(content)
+        if not query_strings:
+            return entries
+        
+        ir = IRController(self.factory)
+        top_results = ir.get_top_results(query_strings)
+        if not top_results:
+            return entries
+        
+        ie = IEController(self.factory, target_format)
+        entries = ie.extract_reference(top_results)
+        return entries
+        
+        
