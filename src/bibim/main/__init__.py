15273f2b1e00ce971ed9de270a45f17e92a9b2f2
rxuriguera <rxuriguera@gmail.com>
2010-02-24 01:01:25 +0100

    Remove imports from main module

--- src/bibim/main/__init__.py
+++ src/bibim/main/__init__.py
@@ -1,7 +1,2 @@
-from __future__ import absolute_import #@UnresolvedImport
-from bibim.main.factory import UtilFactory, UtilCreationError #@UnresolvedImport
-from bibim.main.refmaker import ReferenceMaker #@UnresolvedImport
-from bibim.main.threads import (ThreadRunner, #@UnresolvedImport
-                                ReferenceMakerThread) #@UnresolvedImport
-from bibim.main.bibim import IndexMaker #@UnresolvedImport
+
 
