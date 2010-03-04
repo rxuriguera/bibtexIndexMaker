b588ccd521029b35d5278c2011b3128ba8f718d2
rxuriguera <rxuriguera@gmail.com>
2010-03-04 11:23:05 +0100

    Add searchers for Bing, Yahoo and Google using JSON

--- src/bibim/ir/__init__.py
+++ src/bibim/ir/__init__.py
@@ -1,8 +1,1 @@
-from bibim.ir.search import (SearchError,
-                             ParseError,
-                             SearchResult,
-                             GoogleSearchResult,
-                             ScholarSearchResult,
-                             Searcher,
-                             GoogleSearch,
-                             ScholarSearch)
+
--- src/bibim/ir/search.py
+++ src/bibim/ir/search.py
@@ -27,6 +27,7 @@ import re
 import urllib #@UnresolvedImport
 import time
 import random #@UnresolvedImport
+import simplejson #@UnresolvedImport
 
 from htmlentitydefs import name2codepoint #@UnresolvedImport
 
@@ -81,13 +82,13 @@ class SearchResult(object):
         return 'http://' + self.url.split('/')[2]    
 
 
-class GoogleSearchResult(SearchResult):
+class DescSearchResult(SearchResult):
     
     """
     """
     
     def __init__(self, title, url, desc):
-        super(GoogleSearchResult, self).__init__(title, url)
+        super(DescSearchResult, self).__init__(title, url)
         self.desc = desc
 
 
@@ -109,6 +110,8 @@ class Searcher(object):
     """
     GOOGLE = 0
     SCHOLAR = 1
+    BING = 2
+    YAHOO = 3
     
     def __init__(self, query='', random_agent=False, debug=False):
         self.query = query
@@ -129,6 +132,7 @@ class Searcher(object):
     def set_query(self, value):
         self.__query = value
         self.eor = False
+        self.results_info = None
         
     query = property(get_query, set_query)
             
@@ -191,16 +195,18 @@ class Searcher(object):
             raise cls(*arg)
 
     def _get_safe_url(self):
-        raise NotImplementedError()
+        return self.search_engine_url % {'query':urllib.quote_plus(self.query),
+                                'start':self._page * self._results_per_page,
+                                'num'  :self._results_per_page }
 
     def _get_results_page(self):
         safe_url = self._get_safe_url() 
         
         # Wait a random time between 0.5 and 1,5 seconds before doing the 
         # search
-        time_to_wait = random.randrange(5, 15, 2) / 10.0
-        log.debug('Waiting %g before searching %s' % (time_to_wait, safe_url))
-        time.sleep(time_to_wait)
+        #time_to_wait = random.randrange(5, 15, 2) / 10.0
+        #log.debug('Waiting %g before searching %s' % (time_to_wait, safe_url))
+        #time.sleep(time_to_wait)
         
         try:
             page = self.browser.get_page(safe_url)
@@ -208,14 +214,14 @@ class Searcher(object):
             raise SearchError("Failed getting %s: %s" % (e.url, e.error))
         return BeautifulSoup(page)
 
-    def _extract_info(self, soup):
+    def _extract_info(self, page):
         raise NotImplementedError()
 
-    def _extract_raw_results_list(self, soup):
+    def _extract_raw_results_list(self, page):
         raise NotImplementedError()
 
-    def _extract_results(self, soup):
-        results = self._extract_raw_results_list(soup)
+    def _extract_results(self, page):
+        results = self._extract_raw_results_list(page)
         ret_res = []
         for result in results:
             eres = self._extract_result(result)
@@ -255,22 +261,17 @@ class GoogleSearch(Searcher):
         return ('http://www.google.com/search?hl=en&q=%(query)s&num=%(num)d&st'
                 'art=%(start)d')    
 
-    def _get_safe_url(self):
-        return self.search_engine_url % {'query':urllib.quote_plus(self.query),
-                                'start':self._page * self._results_per_page,
-                                'num'  :self._results_per_page }
-
-    def _extract_info(self, soup):
+    def _extract_info(self, page):
         empty_info = {'from': 0, 'to': 0, 'total': 0}
-        div_ssb = soup.find('div', id='ssb')
+        div_ssb = page.find('div', id='ssb')
         if not div_ssb:
             self._maybe_raise(ParseError, ('Div with number of results was not'
-                ' found on Google search page'), soup)
+                ' found on Google search page'), page)
             return empty_info
         p = div_ssb.find('p')
         if not p:
             self._maybe_raise(ParseError, ('<p> tag within <div id="ssb"> was'
-                ' not found on Google search page'), soup)
+                ' not found on Google search page'), page)
             return empty_info
         txt = ''.join(p.findAll(text=True))
         txt = txt.replace(',', '')
@@ -282,8 +283,8 @@ class GoogleSearch(Searcher):
                 'to': int(matches.group(2)),
                 'total': int(matches.group(3))}
     
-    def _extract_raw_results_list(self, soup):
-        return soup.findAll('li', {'class': 'g'})
+    def _extract_raw_results_list(self, page):
+        return page.findAll('li', {'class': 'g'})
 
     def _extract_result(self, result):
         """
@@ -293,7 +294,7 @@ class GoogleSearch(Searcher):
         desc = self._extract_description(result)
         if not title or not url or not desc:
             return None
-        return GoogleSearchResult(title, url, desc)
+        return DescSearchResult(title, url, desc)
 
     def _extract_title_url(self, result):
         #title_a = result.find('a', {'class': re.compile(r'\bl\b')})
@@ -348,22 +349,17 @@ class ScholarSearch(Searcher):
         return ('http://scholar.google.com/scholar?hl=en&q=%(query)s&num=%'
                 '(num)d&start=%(start)d')
     
-    def _get_safe_url(self):
-        return self.search_engine_url % {'query':urllib.quote_plus(self.query),
-                                'start':self._page * self._results_per_page,
-                                'num'  :self._results_per_page }
-        
-    def _extract_info(self, soup):
+    def _extract_info(self, page):
         empty_info = {'from': 0, 'to': 0, 'total': 0}
-        td_cell = soup.find('td', bgcolor='#dcf6db', align='right')
+        td_cell = page.find('td', bgcolor='#dcf6db', align='right')
         if not td_cell:
             self._maybe_raise(ParseError, ('Cell with number of results was '
-                'not found on Google Scholar search page'), soup)
+                'not found on Google Scholar search page'), page)
             return empty_info
         font = td_cell.find('font')     
         if not font:
             self._maybe_raise(ParseError, ('<font> tag within <td> was not '
-                'found on Google Scholar search page'), soup)
+                'found on Google Scholar search page'), page)
             return empty_info
         txt = ''.join(font.findAll(text=True))
         txt = txt.replace(',', '').split('.')[0]
@@ -376,8 +372,8 @@ class ScholarSearch(Searcher):
                 'to': int(matches.group(2)),
                 'total': int(matches.group(3))}
   
-    def _extract_raw_results_list(self, soup):
-        return soup.findAll('div', {'class': 'gs_r'})    
+    def _extract_raw_results_list(self, page):
+        return page.findAll('div', {'class': 'gs_r'})    
   
     def _extract_result(self, result):
         if not self._check_result(result):
@@ -409,7 +405,6 @@ class ScholarSearch(Searcher):
         return valid
 
     def _extract_title_url(self, result):
-        #title_a = result.find('a', {'class': re.compile(r'\bl\b')})
         title_a = result.find('a')
         if not title_a:
             self._maybe_raise(ParseError, ('Title tag in Google Scholar search'
@@ -496,3 +491,198 @@ class ScholarSearch(Searcher):
          
         desc = ''.join(result_text.findAll(text=True))
         return self._html_unescape(desc.replace('\n', '')).strip()
+    
+
+class JSONSearch(Searcher):
+    
+    def __init__(self, query='', random_agent=False, debug=False):
+        super(JSONSearch, self).__init__(query, random_agent, debug)
+        self.to_path = ''
+        self.from_path = ''
+        self.total_path = ''
+        self.results_path = ''
+        
+        self.result_title = ''
+        self.result_url = ''
+        self.result_desc = ''        
+    
+    def _get_results_page(self):
+        safe_url = self._get_safe_url() 
+        
+        try:
+            page = self.browser.get_page(safe_url)
+        except BrowserError, e:
+            raise SearchError("Failed getting %s: %s" % (e.url, e.error))
+        
+        # Unlike the normal searcher, return the json element
+        return simplejson.loads(page)
+
+    
+    def _extract_raw_results_list(self, page):
+        results = self._extract_field(page, self.results_path)
+        if not results:
+            results = []
+        return results
+  
+    def _extract_result(self, result):
+        title = self._extract_field(result, self.result_title)
+        url = self._extract_field(result, self.result_url)
+        desc = self._extract_field(result, self.result_desc)
+        if not title or not url or not desc:
+            return None
+        return DescSearchResult(title, url, desc) 
+
+    def _extract_info(self, page):
+        empty_info = {'from': 0, 'to': 0, 'total': 0}
+        
+        total = self._extract_field(page, self.total_path)
+        from_result = self._extract_field(page, self.from_path)
+        to_result = self._extract_field(page, self.to_path)
+        
+        return {'from': from_result,
+                'to': to_result,
+                'total': total}
+    
+    def _extract_field(self, root, field):
+        """
+        Extracts a field from the given root element. Field is expressed 
+        as a path.
+        """
+        current = root
+        for element in field.split('/'):
+            try:
+                current = current[element]
+            except KeyError, e:
+                self._maybe_raise(ParseError, ('Could not find field %s' % 
+                                  field), root)
+                return None
+        return current
+
+    
+class GoogleJSONSearch(JSONSearch):
+    """
+    """
+    def __init__(self, query='', random_agent=False, debug=False):
+        super(GoogleJSONSearch, self).__init__(query, random_agent, debug)
+        self.to_path = ''
+        self.from_path = 'responseData/cursor/currentPageIndex'
+        self.total_path = 'responseData/cursor/estimatedResultCount'
+        self.results_path = 'responseData/results'
+        
+        self.result_title = 'titleNoFormatting'
+        self.result_url = 'unescapedUrl'
+        self.result_desc = 'content'
+    
+    @property
+    def search_engine_url(self):
+        return ('http://ajax.googleapis.com/ajax/services/search/web?'
+                'q=%(query)s&rsz=large&hl=en&start=%(start)d&v=1.0')
+
+    def _extract_info(self, page):
+        """
+        Google's JSON output has no 'to' field. This method corrects the
+        behaviour of the parent for this specific case.
+        """
+        results = self._extract_raw_results_list(page)
+        if not len(results):
+            return {'from': 0, 'to': 0, 'total': 0}        
+        
+        info = super(GoogleJSONSearch, self)._extract_info(page)
+        
+        info['total'] = int(info['total'])
+        if info['from'] >= 0:
+            info['from'] = info['from'] * 8 + 1
+        info['to'] = info['from'] + len(results)
+        if info['to'] > 0:
+            info['to'] -= 1
+            
+        return info
+
+
+class YahooSearch(JSONSearch):
+    """
+    """
+    def __init__(self, query='', random_agent=False, debug=False):
+        super(YahooSearch, self).__init__(query, random_agent, debug)
+        self.to_path = 'ysearchresponse/count'
+        self.from_path = 'ysearchresponse/start'
+        self.total_path = 'ysearchresponse/totalhits'
+        self.results_path = 'ysearchresponse/resultset_web'
+        
+        self.result_title = 'title'
+        self.result_url = 'url'
+        self.result_desc = 'abstract'
+    
+    @property
+    def search_engine_url(self):
+        return ('http://boss.yahooapis.com/ysearch/web/v1/%(query)s?'
+                'appid=waXcSVvV34FGdFtiMPDBLnhqsMlY684wiZdDNVSLzmJ7CYBc8FInS2'
+                'rR6ye4xzeP_Q--&type=html,-pdf&'
+                'count=%(num)d&start=%(start)d')
+
+    def _extract_info(self, page):
+        """
+        Google's JSON output has no 'to' field. This method corrects the
+        behaviour of the parent for this specific case.
+        """
+        empty_info = {'from': 0, 'to': 0, 'total': 0}        
+        
+        results = self._extract_raw_results_list(page)
+        if not len(results):
+            return empty_info      
+        
+        total = int(self._extract_field(page, self.total_path))
+        from_result = int(self._extract_field(page, self.from_path))
+        to_result = from_result + int(self._extract_field(page, self.to_path))
+        
+        if to_result  and total:
+            from_result += 1
+        
+        return {'from': from_result,
+                'to': to_result,
+                'total': total}
+
+class BingSearch(JSONSearch):
+    """
+    """
+    def __init__(self, query='', random_agent=False, debug=False):
+        super(BingSearch, self).__init__(query, random_agent, debug)
+        self.to_path = 'SearchResponse/count'
+        self.from_path = 'SearchResponse/Web/Offset'
+        self.total_path = 'SearchResponse/Web/Total'
+        self.results_path = 'SearchResponse/Web/Results'
+        
+        self.result_title = 'Title'
+        self.result_url = 'Url'
+        self.result_desc = 'Description'
+    
+    @property
+    def search_engine_url(self):
+        return ('http://api.bing.net/json.aspx?Query=%(query)s'
+                '&Web.Count=%(num)d&Web.Offset=%(start)d&Market=en-us'
+                '&Web.Options=DisableHostCollapsing+DisableQueryAlterations'
+                '&AppId=70E17856170345A262CE7E76C119B66CF492E924&Sources=Web'
+                '&Version=2.2')    
+        
+    def _extract_info(self, page):
+        """
+        Google's JSON output has no 'to' field. This method corrects the
+        behaviour of the parent for this specific case.
+        """
+        empty_info = {'from': 0, 'to': 0, 'total': 0}        
+        
+        results = self._extract_raw_results_list(page)
+        if not len(results):
+            return empty_info      
+        
+        total = int(self._extract_field(page, self.total_path))
+        from_result = int(self._extract_field(page, self.from_path))
+        to_result = from_result + len(results)
+        
+        if to_result  and total:
+            from_result += 1
+        
+        return {'from': from_result,
+                'to': to_result,
+                'total': total}
+        
