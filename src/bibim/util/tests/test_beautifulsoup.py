413eead918d5c91246c1a9ccdf7ae5b08a70d1d9
rxuriguera <rxuriguera@gmail.com>
2010-03-12 11:51:22 +0100

    Added a couple of tests for BeautifulSoup

---
+++ src/bibim/util/tests/test_beautifulsoup.py
@@ -0,0 +1,45 @@
+
+# Copyright 2010 Ramon Xuriguera
+#
+# This file is part of BibtexIndexMaker.
+#
+# BibtexIndexMaker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BibtexIndexMaker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BibtexIndexMaker. If not, see <http://www.gnu.org/licenses/>.
+
+
+import unittest #@UnresolvedImport
+
+from bibim.util.beautifulsoup import BeautifulSoup 
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_create_soup_from_None(self):
+        self.failUnlessRaises(TypeError, BeautifulSoup, None)
+
+    def test_create_soup_from_empty_string(self):
+        try:
+            soup = BeautifulSoup('')
+            self.failIf(soup is None)
+        except:
+            self.fail("Soup of empty string shouldn't raise an exception")
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
