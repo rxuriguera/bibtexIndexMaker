rxuriguera/bibtexIndexMaker

commit ffb2432b21c5cbc3bbde7240f62db4574b58f99d

Simplify the way results are added.
Updated parameters for adding used queries and results

src/bibim/main/persistence.py
@@ -46,12 +46,12 @@ class Persistor(object):
         publication.add_search_results([mappers.Result(result.url) for result in dto.top_results])
         
         if dto.used_query:
-            publication.add_query_strings(mappers.QueryString(dto.used_query))
+            publication.add_query_strings(mappers.QueryString(dto.used_query,
+                                                              True))
         
         if dto.used_result:
-            used_result = mappers.Result(dto.used_result.url)
-            used_result.publication = publication
-            publication.add_search_results(used_result)
+            publication.add_search_results(mappers.Result(dto.used_result.url,
+                                                          True))
         
         # One single publication can have more than one entry (e.g. inbook + book)
         for entry in dto.entries:
@@ -70,7 +70,6 @@ class Persistor(object):
                 else:
                     reference.add_field(field.name, field.value, field.valid)
                     
-            reference.set_result(used_result)
             publication.add_reference(reference)
 
 

commit 681dcca27f766d29cb7a6f00ea55a05dc14d1840

Fixed regression error introduced by ffb2432b21c

src/bibim/main/persistence.py
@@ -39,6 +39,7 @@ class Persistor(object):
         """
         Stores all the information of a processed file to the database.
         """
+        used_result = None
         publication = mappers.Publication(file=unicode(dto.file))
         self.session.add(publication)
     
@@ -50,13 +51,19 @@ class Persistor(object):
                                                               True))
         
         if dto.used_result:
-            publication.add_search_results(mappers.Result(dto.used_result.url,
-                                                          True))
+            used_result = mappers.Result(dto.used_result.url, True)
+            used_result.publication = publication
+            publication.add_search_results(used_result)
         
         # One single publication can have more than one entry (e.g. inbook + book)
         for entry in dto.entries:
             reference = mappers.Reference()
             reference.valid = entry.is_valid()
+            
+            # Set result_id key for the reference
+            if used_result:
+                reference.set_result(used_result)
+                
             for field in entry.get_fields():
                 field = entry.get_field(field)
                 