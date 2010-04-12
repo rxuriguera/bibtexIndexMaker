aa6d90cdb68aa76c48ebc2ef91f4c0c939f5c2f7
rxuriguera <rxuriguera@gmail.com>
2010-04-13 01:15:28 +0200

    Removed validation code from trainers

--- src/bibim/ie/trainers.py
+++ src/bibim/ie/trainers.py
@@ -16,21 +16,26 @@
 # You should have received a copy of the GNU General Public License
 # along with BibtexIndexMaker. If not, see <http://www.gnu.org/licenses/>.
 
-from math import ceil, floor
-
 from bibim.ie.examples import HTMLExampleManager
 from bibim.ie.rules import PathRuler, RegexRuler
+from bibim.ie.wrappers import RuledWrapper
 
 class TooFewExamplesError(Exception):
     pass
 
 
 class WrapperTrainer(object):
-    def __init__(self, example_manager, rulers=[], tp=0.8, vp=0.2, min=2):
+    """
+    This is a superclass for training ruled wrappers. 
+    The given ExampleManager has to be compatible with the rulers in order
+    for the trainer to work properly.
+    The list of rulers must be ordered the same way as the rules will be 
+    applied by the wrapper.
+    """
+    
+    def __init__(self, example_manager, rulers=[], min=2):
         self.example_manager = example_manager
         self.rulers = rulers
-        self.training_percentage = tp
-        self.validation_percentage = vp
         self.min_examples = min
 
     def get_example_manager(self):
@@ -38,12 +43,6 @@ class WrapperTrainer(object):
 
     def get_rulers(self):
         return self.__rulers
-
-    def get_training_percentage(self):
-        return self.__training_percentage
-
-    def get_validation_percentage(self):
-        return self.__validation_percentage
 
     def get_min_examples(self):
         return self.__min_examples
@@ -54,63 +53,44 @@ class WrapperTrainer(object):
     def set_rulers(self, value):
         self.__rulers = value
 
-    def set_training_percentage(self, value):
-        self.__training_percentage = value
-
-    def set_validation_percentage(self, value):
-        self.__validation_percentage = value
-
     def set_min_examples(self, value):
         self.__min_examples = value
 
     example_manager = property(get_example_manager, set_example_manager)
     rulers = property(get_rulers, set_rulers)
-    training_percentage = property(get_training_percentage, set_training_percentage)
-    validation_percentage = property(get_validation_percentage, set_validation_percentage)
     min_examples = property(get_min_examples, set_min_examples)
     
-    def train(self):
-        raise NotImplementedError
+    def train(self, example_sets):   
+        wrapper = RuledWrapper()     
+        for set in example_sets:
+            rules = self._train_field(example_sets[set])                    
+            if rules:
+                wrapper.add_field_rules(set, rules)
+        return wrapper        
+
+    def _train_field(self, examples):        
+        if len(examples) < self.min_examples:
+            raise TooFewExamplesError
+        
+        rules = []
+        for ruler in self.rulers:
+            rules.append(ruler.rule(examples))
+        return rules
 
 
 class HTMLWrapperTrainer(WrapperTrainer):
     """
-    This wrapper trainer will generate wrappers 
+    This wrapper trainer will generate wrappers for HTML documents. These kind
+    of documents need two rules: path rules and regex rules.
     """
     
-    def __init__(self, tp=0.8, vp=0.2, min=2):
+    def __init__(self, min=2):
         super(HTMLWrapperTrainer, self).__init__(HTMLExampleManager(),
                                                  [PathRuler(), RegexRuler()],
-                                                 tp, vp, min)
+                                                 min)
         
     def train(self, url):
-        # HTMLExampleManager returns different sets of examples. We have 
-        # to induce rules for each of them.
-        example_sets = self.example_manager.get_examples(url, self.min_examples)
-    
-        for set in example_sets:
-            training, validation = self._split_examples(example_sets[set])
-            
-            for ruler in self.rulers:
-                rule = ruler.rule(training)
-                
-    
-    def _split_examples(self, examples):
-        """
-        This method splits a set of examples into two: the training and 
-        validation sets. It returns a list of two elements with the two sets
-        of examples.
-        """
-        if len(examples) < self.min_examples:
-            raise TooFewExamplesError
+        example_sets = self.example_manager.get_examples(url,
+                                                         self.min_examples)
+        return super(HTMLWrapperTrainer, self).train(example_sets)
         
-        training = list(examples)
-        
-        validation = set()
-        validation_length = int(ceil(self.validation_percentage * 
-                                     len(examples)))
-        for i in range(validation_length): 
-            validation.add(training.pop())
-        
-        return (training, validation)
-    
