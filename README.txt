b975b929008a84104341189f51a1d652092bfd75
rxuriguera <rxuriguera@gmail.com>
2010-06-23 12:26:14 +0200

    Update setup script

---
+++ README.txt
@@ -0,0 +1,12 @@
+About
+-------------------------------------------------------------------------------
+BibTeX Bibliograpny Index Maker is an open source reference management 
+application that focuses on the creation of lists of BibTeX references 
+from a collection of PDF files containing scientific articles.
+
+
+How does it work?
+-------------------------------------------------------------------------------
+For each PDF file, BibTeX Inex Maker uses xPDF’s command line tools to extract 
+its text. It then searches the Web for pages with information about the article 
+and tries to extract reference from them.
--- setup.py
+++ setup.py
@@ -1,17 +1,50 @@
+import ez_setup
+ez_setup.use_setuptools()
+
 from setuptools import setup, find_packages
+import glob
+import platform #@UnresolvedImport
 import sys, os
 
+_tool_path_prefix = 'tools/xpdf/'
+_tool_path = {
+    'darwin':'linux/pdftotext', # MAC OS 
+    'linux':'linux/pdftotext',
+    'windows':'windows/pdftotext.exe'
+}
+
+platform = platform.system().lower()
+if platform not in _tool_path.keys():
+    tool = ''.join([_tool_path_prefix, 'linux'])
+else:
+    tool = ''.join([_tool_path_prefix, platform])
+    
 version = '0.0'
 
 setup(name='bibim',
       version=version,
       description="Bibtex Bibliography Index Maker",
       license='GPL',
-      packages=find_packages('src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      keywords='bibtex pdf reference citation index ',
+      url='http://github.com/rxuriguera/bibtexIndexMaker',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: X11 Applications :: Qt',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+      ],
+      packages=find_packages('src', exclude=["*.tests", "*.tests.*", "tests.*", "tests", "benchmark.*"]),
       package_dir={'':'src'},
       install_requires=['setuptools', 'simplejson', 'sqlalchemy'],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      include_package_data=True,
+      data_files=[('config', glob.glob('config/*.cfg')),
+                  (tool, glob.glob(''.join([tool, '/*'])))],
+      entry_points={
+        'gui_scripts': [
+            'bibim = bibim.gui.main.main',
+        ],
+        'setuptools.installation': [
+            'eggsecutable = bibim.gui.main:main',
+        ]
+    }
 )
-
